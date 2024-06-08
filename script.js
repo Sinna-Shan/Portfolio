70b9a18a68036466b97734aa5a352ed831102748
Noah Shan <luth595@gmail.com>
2024-06-08 16:07:30 +0530

    email send function and footer

--- script.js
+++ script.js
@@ -8,3 +8,34 @@ document
       document.querySelector(id).scrollIntoView({ behavior: "smooth" });
     }
   });
+
+// *******************
+//    EMAIL SCRIPT
+// *******************
+
+const from = document.getElementById("from_name");
+const email = document.getElementById("email_id");
+const message = document.getElementById("msg");
+
+document.querySelector(".btn-form-sub").addEventListener("click", (e) => {
+
+  if(!(from.value || from.value || message.value)) {
+    alert("Please fill all the fields");
+    return;
+  }
+
+  const params = {
+    from_name: from.value,
+    email_id: email.value,
+    message: message.value,
+  };
+
+
+  emailjs
+    .send("service_xl3e22g", "template_z6f5vuo", params)
+    .then((response) => {
+      alert("I am glad to here from you. get in touch with you soon!");
+    });
+
+
+});
