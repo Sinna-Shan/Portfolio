9c6506b0c70cedadfd9c185beedfd2a470ef9806
Noah Shan <luth595@gmail.com>
2024-07-04 19:55:05 +0530

    projects section

--- js/script.js
+++ js/script.js
@@ -7,6 +7,35 @@ btnNav.addEventListener("click", () => {
   headerEl.classList.toggle("nav-open");
 });
 
+// smooth scrolling
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // scroll to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      document.querySelector(href).scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+
+    // close mobile nav
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
+
 
 //=================================================
 // sticky navigation
