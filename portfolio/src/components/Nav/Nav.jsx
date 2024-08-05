a74fc766c8afe900492161dcd6f75a2fc6edaf36
Noah Shan <luth595@gmail.com>
2024-08-05 20:23:00 +0530

    responsive navigation

--- portfolio/src/components/Nav/Nav.jsx
+++ portfolio/src/components/Nav/Nav.jsx
@@ -1,24 +1,40 @@
+import { useRef } from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./Nav.module.css"
 
 function Nav() {
+  const navRef = useRef()
+
+  function showNavBar() {
+    navRef.current.classList.toggle(styles.navOpen);
+  }
   return (
-    <nav className={styles.nav}>
-      <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/about">About</NavLink>
-        </li>
-        <li>
-          <NavLink to="/projects">Projects</NavLink>
-        </li>
-        <li>
-          <NavLink to="/contact">Contact</NavLink>
-        </li>
-      </ul>
-    </nav>
+    <header className={styles.header} ref={navRef}>
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/projects">Projects</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <div className={styles.mobileNav}>
+        <button className={styles.menuBtn} onClick={showNavBar} name="close">
+          <img src="close.svg" alt="close button" />
+        </button>
+        <button className={styles.menuBtn} onClick={showNavBar} name="open">
+          <img src="menu.svg" alt="menu button" />
+        </button>
+      </div>
+    </header>
   );
 }
 
